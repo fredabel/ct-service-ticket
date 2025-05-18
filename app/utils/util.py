31dce07689e7bef8c0d18ce320c9ee12f88b0943
fredabel <tuazonpred@gmail.com>
2025-05-18 19:10:21 -0400

    Login method for both mechanic and customer

--- app/utils/util.py
+++ app/utils/util.py
@@ -6,11 +6,12 @@ from flask import request, jsonify
 
 SECRET_KEY = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
-def encode_token(customer_id): #using unique pieces of info to make our tokens user specific
+def encode_token(user_id, user_type): 
     payload = {
-        'exp': datetime.now(timezone.utc) + timedelta(days=0,hours=1), #Setting the expiration time to an hour past now
-        'iat': datetime.now(timezone.utc), #Issued at
-        'sub':  str(customer_id) #This needs to be a string or the token will be malformed and won't be able to be decoded.
+        'exp': datetime.now(timezone.utc) + timedelta(days=0,hours=1), 
+        'iat': datetime.now(timezone.utc), 
+        'sub': str(user_id),  # User ID
+        'user_type': user_type  # ("customer" or "mechanic")
     }
 
     token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
@@ -22,22 +23,22 @@ def token_required(f):
         token = None
         # Look for the token in the Authorization header
         if 'Authorization' in request.headers:
-            token = request.headers['Authorization'].split(" ")[1]
-        
+            token = request.headers['Authorization'].split()[1]
+            
         if not token:
             return jsonify({'message': 'Token is missing!'}), 401
 
         try:
             # Decode the token
             data = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
-            customer_id = data['sub']  # Fetch the user ID
-            
+            request.userid = int(data['sub'])  # Fetch the user ID
+            request.user_type = data['user_type']  # Fetch the user type
         except jose.exceptions.ExpiredSignatureError:
              return jsonify({'message': 'Token has expired!'}), 401
         except jose.exceptions.JWTError:
              return jsonify({'message': 'Invalid token!'}), 401
 
-        return f(customer_id, *args, **kwargs)
+        return f(*args, **kwargs)
 
     return decorated
 
