496ad8d4e144dc148ebced44637f3a722d9c0bea
fredabel <tuazonpred@gmail.com>
2025-05-28 14:09:32 -0400

    Update swagger to local

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from app import create_app
 from app.models import db
+from flask_cors import CORS
 
 app = create_app('DevelopmentConfig')
-
+CORS(app)
 with app.app_context():
     # db.drop_all()
     db.create_all()
